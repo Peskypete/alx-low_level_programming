999bb3d51e6d71d67f45f2d138eaeb7c2ca6b2e5
Peskypete <mwololopeter777@gmail.com>
2023-03-17 04:19:58 -0700

    the 6 file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ float floatType;
 printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 printf("Size of an int: %lu byte(s)\n", sizeof(integerType));
 printf("Size of a long int: %llu byte(s)\n", sizeof(longintType));
-printf("Size of long long int: %llu byte(s)\n", sizeof(longlongintType));
+printf("Size of a long long int: %llu byte(s)\n", sizeof(longlongintType));
 printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 return (0);
 }
