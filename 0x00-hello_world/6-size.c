94de1f15706270fc64079e9227e66d12a6eebe6c
Peskypete <mwololopeter777@gmail.com>
2023-03-19 05:58:43 -0700

    Have to understand gcc -32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int integerType;
 long int longintType;
 long long int longlongintType;
 float floatType;
-printf("Size of a char: %lu byte(s)\n",(unsigned long) sizeof(charType));
-printf("Size of an int: %lu byte(s)\n",(unsigned long) sizeof(integerType));
-printf("Size of a long int: %llu byte(s)\n",(unsigned long) sizeof(longintType));
-printf("Size of a long long int: %llu byte(s)\n",(unsigned long) sizeof(longlongintType));
-printf("Size of a float: %lu byte(s)\n",(unsigned long) sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(charType));
+printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(integerType));
+printf("Size of a long int: %llu byte(s)\n", (unsigned long) sizeof(longintType));
+printf("Size of a long long int: %llu byte(s)\n", (unsigned long) sizeof(longlongintType));
+printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(floatType));
 return (0);
 }
