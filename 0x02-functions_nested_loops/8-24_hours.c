c6df930cd838e9e028ee35162167962f3ae2ec06
Peskypete <mwololopeter777@gmail.com>
2023-03-21 14:00:32 -0700

    walking on water

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *jack_bauer -  prints every minute of the day
+ *Return: ...
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+	for (a = 0; a <= 2; a++)
+	{
+	for (b = 0; b <= 4; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a);
+	_putchar(b);
+	_putchar(':');
+	_putchar(c);
+	_putchar(d);
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
