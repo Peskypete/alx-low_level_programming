4f9e8cfc7fda2637f1ad19ba0d12d37aedf6c968
Peskypete <mwololopeter777@gmail.com>
2023-03-21 15:24:42 -0700

    motema

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 : prints the alphabet ten times
+ * print_alphabet_x10 - prints the alphabet ten times
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
