7e28881c541678a0da160b1679c95828e2707b98
Peskypete <mwololopeter777@gmail.com>
2023-03-21 15:12:41 -0700

    AnoTher DimEnSIOn

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,8 @@ int print_last_digit(int n)
 	last_d = n % 10;
 	if (last_d < 0)
 	{
-	last_d *= -1;
-	_putchar(last_d + '0');
+	last_d = last_d * -1;
 	}
+	_putchar(last_d + '0');
 	return (last_d);
 }
