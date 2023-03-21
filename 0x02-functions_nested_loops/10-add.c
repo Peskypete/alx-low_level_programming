a254ac2d6a31c12714835e4ed5eac154ae285df1
Peskypete <mwololopeter777@gmail.com>
2023-03-21 14:59:29 -0700

    ni kumoto manze

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - Adds two values
+ *@n1: The first digit to be computed
+ *@n2: The second digit to be computed
+ *Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
