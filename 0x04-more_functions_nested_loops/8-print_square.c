27cba276655b7af36471001c2e91fec9ad715d47
Peskypete <mwololopeter777@gmail.com>
2023-03-23 17:32:55 -0700

    Aicha

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_square - prints a square
- *@size ; the size of the square
+ *@size: the size of the square
  *Return: no arguments
  */
 void print_square(int size)
@@ -21,8 +21,8 @@ void print_square(int size)
 	for (j = 0; j < size; j++)
 	{
 	_putchar(35);
-	_putchar('\n');
 	}
+	_putchar('\n');
 	}
 	}
 }
