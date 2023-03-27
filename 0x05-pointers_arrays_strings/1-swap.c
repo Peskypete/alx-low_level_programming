6aacc8e6f8d4f7d318836a270ce74292e777a65a
Peskypete <mwololopeter777@gmail.com>
2023-03-27 15:03:13 -0700

    okay okayy

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *swap_int
- *@*a: the first to be dereferenced
- *@*b: the second to be dereferenced
+ *swap_int - swaps integer values
+ *@a: the first to be dereferenced
+ *@b: the second to be dereferenced
  *void: returns nothing
  */
 void swap_int(int *a, int *b)
