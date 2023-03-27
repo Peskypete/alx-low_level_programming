ca994aedfce326657fa9fccbbee8404576d70000
Peskypete <mwololopeter777@gmail.com>
2023-03-27 15:38:52 -0700

    AAh MoTemA na NGaA

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,13 @@
 /**
  * _strcpy - copy a string
  * @dest: destination value
- * @src: source value 
+ * @src: source value
  * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	dest[i] = src[i];
 
