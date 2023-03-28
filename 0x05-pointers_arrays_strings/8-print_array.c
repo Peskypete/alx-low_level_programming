235a8674a95ec6647b26814a4779e3caa4908b94
Peskypete <mwololopeter777@gmail.com>
2023-03-28 09:24:27 -0700

    VOILa

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- *
+ *print_array - prints array contents
+ *@a: pointer 
+ *n:number of variable values to be printed
  */
 void print_array(int *a, int n)
 {
 	int c;
-
+	if (n > 0)
+	{
 	for (c = 0; c < n; c++)
 	{
 	if (c == n - 1)
@@ -15,6 +18,14 @@ void print_array(int *a, int n)
 	else
 	{
 	printf("%d, ", a[c]);
+	}
+	}
+	}
+	else
+	{
+	for (; n <= 0; n++)
+	{
+	printf("%d", a[n]);
 	}
 	}
 }
