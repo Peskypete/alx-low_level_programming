eb2286e1413241d40a003563a2ca7c4fcfd3afd4
Peskypete <mwololopeter777@gmail.com>
2023-03-29 23:10:52 -0700

    YEA YEA

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array of type int
+ * @a: the array 
+ * @n: number of elements in array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int z;
+	int index = n - 1;
+	int half = n / 2;
+
+	for (z = 0; z <= half; z++)
+	{
+	int j = a[z];
+	a[z] = a[index - z];
+	a[index - z] = j;
+	}
+}
