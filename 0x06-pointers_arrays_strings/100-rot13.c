7f7baac690e1f0cd2512e6d2a552750e63c3f2c7
Peskypete <mwololopeter777@gmail.com>
2023-04-01 03:35:07 -0700

    To God be the Glory GREAT THINGS HE HATH DONE

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes letter into a letter 13 places into another letter
+ * @str: the string
+ * Return: an adress to str
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char s1[] = "aABbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char s2[] = "nNOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for(i = 0; str[i] != '\0'; i++)
+	{
+	j = 0;
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		if (str[i] == s1[j])
+		{
+			str[i] = s2[j];
+		}
+	}
+	}
+	return (str);
+}
