8cfa77cdffb6a2b4edb6fd8ce6afdc319a14118d
Peskypete <mwololopeter777@gmail.com>
2023-04-03 02:43:57 -0700

    Body & Soul

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,6 +3,8 @@
 /**
  * _memcpy - copies n bytes from string src to char
  * @n: the number of bytes to be copied
+ * @src: the string to be copied
+ * @dest: where the string src should be copied
  * Return: a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
