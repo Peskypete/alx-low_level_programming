ed7b667c574ed928be55a4abe4b88a5706c2d719
Peskypete <mwololopeter777@gmail.com>
2023-04-03 03:36:58 -0700

    ONLY YOU

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 	int i = strlen(s);
 	int j;
 
-	for (j = 0; j < i; j++)
+	for (j = 0; j <= i; j++)
 	{
 		if (s[j] == c)
-		return (s);
+		return (s + j);
 	}
 	return (NULL);
 }
