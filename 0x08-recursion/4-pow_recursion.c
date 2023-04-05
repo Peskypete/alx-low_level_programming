42578b37371c117aac023dd949311918f34684fd
Peskypete <mwololopeter777@gmail.com>
2023-04-05 14:44:03 -0700

    sometimes MAY be gOOd SOMEtimES MAYBE SHIT

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - powers a base with an exponent
+ * @x: the base
+ * @y: the exponent
+ * Return: the value after arithmation
+ */
+int _pow_recursion(int x, int y)
+{
+	if(y == 0)
+	{
+		return 1;
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
