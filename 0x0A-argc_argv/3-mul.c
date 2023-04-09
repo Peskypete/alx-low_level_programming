c5cb23a332befff0158d1e34a25f335afe5baf8e
Peskypete <mwololopeter777@gmail.com>
2023-04-09 14:38:16 -0700

    MTOTO WA MWENZIO

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument names
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	if (argc != 3)
+	{
+	printf("Error");
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+	return (0);
+}
