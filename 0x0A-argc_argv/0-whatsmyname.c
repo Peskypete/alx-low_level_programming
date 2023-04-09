1c5d4aaf0e6ce899935c10b6ab4678e08ec3fd4b
Peskypete <mwololopeter777@gmail.com>
2023-04-09 14:09:36 -0700

    pepele

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints name of argv[0]
- * argc: the count of arguments passed
- * argv: the string argument names
+ * @argc: the count of arguments passed
+ * @argv: the string argument names
  * Return: 0 always success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
