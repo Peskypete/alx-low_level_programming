70c583db1fc4ad5f0f99396dbf480e750c9ce2c8
Peskypete <mwololopeter777@gmail.com>
2023-04-10 14:07:24 -0700

    MOTEMA

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,29 +5,38 @@
  * main - adds positive numbers
  * @argc: The argument count
  * @argv: argument names
- * Return: 0 always success
+ * Return: 0 always succes
  */
 int main(int argc, char *argv[])
 {
-	int i, z, sum;
+	int total = 0;
+	int num;
+	int j;
+	int i;
 
-	z = 0;
 	if (argc == 1)
 	{
-	printf("%d\n", z);;
+	printf("0\n");
+	return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(argv[i]))
-		{
-		printf("Error");
-		return (0);
-		}
-		else
-		{
-		sum += atoi(argv[i]);
-		}
+	char *arg = argv[i];
+
+	for (j = 0; arg[j] != '\0'; j++)
+	{
+	if (!isdigit(arg[j]))
+	{
+	printf("Error\n");
+	return (1);
 	}
-	printf("%d\n", sum);
+	}
+	num = atoi(arg);
+	if (num > 0)
+	{
+	total += num;
+	}
+	}
+	printf("%d\n", total);
 	return (0);
 }
