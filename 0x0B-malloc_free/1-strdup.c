94632cca6a25c016101458f86f7505f19529e1c3
Peskypete <mwololopeter777@gmail.com>
2023-04-12 12:54:26 -0700

    KATCHUA

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,11 @@ char *_strdup(char *str)
 	int len = strlen(str);
 	char *st = (char *)malloc((len + 1) * sizeof(char));
 
-	if (st == NULL || str == NULL)
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	if (st == NULL)
 	{
 	return (NULL);
 	}
