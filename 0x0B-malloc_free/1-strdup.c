66f8e7884792a88030c896a819eb8dc21d9fa3f2
Peskypete <mwololopeter777@gmail.com>
2023-04-12 12:39:32 -0700

    SPRIT lead me where my trust is without borders

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strdup - create and replicate passed string
+ * @str: the string to be replicated
+ * Return:  function returns a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int len = strlen(str);
+	char *s = (char *)malloc((len + 1) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(s, str);
+	return (s);
+}
