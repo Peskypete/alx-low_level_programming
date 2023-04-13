941fe0e77aa1e3e6bc03666bb792597cd0f0b5fd
Peskypete <mwololopeter777@gmail.com>
2023-04-13 11:40:41 -0700

    Believe in He who went before us

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates strings into one
+ *@s1: string one
+ *@s2: string two
+ *@n: the number of bytes in s2
+ *Return: a pointer if successfull, NULL otherwise
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	strcpy(s, s1);
+	if (n >= strlen(s2))
+		strcat(s, s2);
+	else
+		strncat(s, s2, n);
+	return (s);
+}
