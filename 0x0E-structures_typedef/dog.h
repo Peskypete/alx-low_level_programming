78f8d0f46eacb7c55c1e628dc148284e4a66844a
Peskypete <mwololopeter777@gmail.com>
2023-04-17 22:12:12 -0700

    panik

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-/*
+/**
  * struct dog - creates a struct named dog
  * @name: name of dog
  * @age: age of dog
  * @owner: of dog
+ * Description: Longer description
  */
 struct dog
 {
@@ -14,6 +15,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
