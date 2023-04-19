0f968d7301b950ab455888dbee6091c070ed5354
Peskypete <mwololopeter777@gmail.com>
2023-04-19 06:33:49 -0700

    nice!

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - print name
+ * @name: the name to be printed
+ * @f: pointer to a function to print
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+#endif
