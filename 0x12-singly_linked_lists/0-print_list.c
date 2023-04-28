7ee8bf6c1a95dee82653aa933bef81e5160065af
Peskypete <mwololopeter777@gmail.com>
2023-04-28 03:27:47 -0700

    champagne

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * print_list - prints data contained
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+	const list_t *temp = h;
+
+	while (temp != NULL)
+	{
+		i++;
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", temp->len, temp->str);
+		}
+		temp = temp->next;
+	}
+	return (i);
+}
