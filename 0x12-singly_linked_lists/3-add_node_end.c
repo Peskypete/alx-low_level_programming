f359e98a9e797fddb1d66ac76cdfc51f55f31fed
Peskypete <mwololopeter777@gmail.com>
2023-04-29 15:19:47 -0700

    wonder

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,10 @@
 #include "lists.h"
 #include <string.h>
 /**
- *
+ * add_node_end - adds node at the end
+ * @str: the string to be duplicated
+ * @head: a pointrer to a pointer to head
+ * Return: a pointer to new_node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
