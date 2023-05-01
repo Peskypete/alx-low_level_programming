925bb64334c244d0e8a20e685e4aae9bbc1d2a2a
Peskypete <mwololopeter777@gmail.com>
2023-05-01 05:15:50 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_listint - freeing a linked list
+ * @head: pointer to head
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *curr;
+	listint_t *next;
+
+	curr = head;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		free(curr->next);
+		free(curr);
+		curr = next;
+	}
+}
