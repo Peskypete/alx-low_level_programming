079cd71191e5e38a6240623c37bdb497ed620bc8
Peskypete <mwololopeter777@gmail.com>
2023-05-04 12:09:56 -0700

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int pow2(int num1 , int exp);
+int _putchar(char c);
 #endif
